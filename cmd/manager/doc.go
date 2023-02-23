e4bd3ddb8b10c411707feb0dae789e975a3e6225
David Bainbridge <dbainbri@ciena.com>
2023-02-23 11:25:45 -0500

    update to newer lint and go version

--- cmd/manager/doc.go
+++ cmd/manager/doc.go
@@ -13,5 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-// main package for the pallet controller
+
+// Package main package for the pallet controller
 package main
